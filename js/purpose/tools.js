06458d3c791a5012bb13b8ef34f2a306c3c0c229
Matt Zhang <set.stun@gmail.com>
2024-12-31 00:39:37 -0600

    More work on Purpose

---
+++ js/purpose/tools.js
@@ -0,0 +1,22 @@
+//<div class="tool">
+    //<h1>Prioritizer</h1>
+    //type=chat
+    //Life is a confusing mess of overlapping objectives. You must balance experiencing the present with planning for the future. Sometimes we can spend years pursuing the task in front of us, and put off the goals that truly matter. Purpose helps you set your true life objectives.
+
+    //(Unsatisfactory friendships/ feel like my job is meaningless/ not enough mental space/ feeling insecure about the future/ not following true passion/ I’ve never explored the world/ when will I write that book?/ I don’t feel personal connection/ everything is boring) - phrase as “I want ….”?
+
+    //Build trees of primary and secondary and tertiary motivations
+
+    //Work late - you want to earn money to retire early and spend more time with your family, yet you are missing your son’s school event to work late. Estimate of reward vs loss. Do you… yes/no
+//</div>
+
+
+
+
+
+//<h1>Change Your Habits</h1>
+//Purpose helps you set new patterns of thought and behavior - be more aligned with your ideal existence
+
+//Uses techniques from cbt, stoicism, etc to help you navigate life's challenges
+
+    //Alert - Your mood has been consistently low recently - You are nearing burnout! The last time this happened, your productivity dropped by 80% for three months. - See suggested actions
---
+++ storage/ideas/pricing.md
@@ -0,0 +1,1 @@
+$20 a month, $200 a year, 1 month trial - half off for life for alpha customers
